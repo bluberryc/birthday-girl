ac3353b30446b98b8dd4441497a9091655228adb
bluberryc <122817968+bluberryc@users.noreply.github.com>
2024-12-08 00:12:29 +0200

    Create wordlist.js

---
+++ wordlist.js
@@ -0,0 +1,51 @@
+
+const wordList = [
+    {
+        word: "lotus",
+        hint: "The map that started it all."
+    },
+    {
+        word: "blueberry",
+        hint: "The best fruit in the world."
+    },
+    {
+        word: "you",
+        hint: "All i want for Christmas is..."
+    },
+    {
+        word: "fearless",
+        hint: "Head first..."
+    },
+    {
+        word: "hesburger",
+        hint: "The very first place we shared a meal together."
+    },
+    {
+        word: "enchanted",
+        hint: "I was ... to meet you <3"
+    },
+    {
+        word: "princess",
+        hint: "You are my..."
+    },
+    {
+        word: "eighteenth",
+        hint: "The day we got officially together."
+    },
+    {
+        word: "home",
+        hint: "I feel at ... when I'm with you <3"
+    },
+    {
+        word: "addict",
+        hint: "The very first song you ever showed me."
+    },
+    {
+        word: "monopoly",
+        hint: "A board game we like to play."
+    },
+    {
+        word: "varna",
+        hint: "The place we met for the first time."
+    },
+];
